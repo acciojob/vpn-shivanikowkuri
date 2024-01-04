c47ca24ef4c693dddfa862c7e8ade96ca18f92b1
shivanikowkuri <147936662+shivanikowkuri@users.noreply.github.com>
2024-01-04 23:27:29 +0530

    Update Country.java

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -1,1 +1,71 @@
 // Note: Do not write @Enumerated annotation above CountryName in this model.
+package com.driver.model;
+
+import javax.persistence.*;
+
+// Note: Do not write @Enumerated annotation above CountryName in this model.
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    CountryName countryName;
+    String code;
+    @OneToOne
+    @JoinColumn
+    User user;
+    @ManyToOne
+    @JoinColumn
+    ServiceProvider serviceProvider;
+
+    public Country() {
+    }
+
+    public Country(int id, CountryName countryName, String code, User user, ServiceProvider serviceProvider) {
+        this.id = id;
+        this.countryName = countryName;
+        this.code = code;
+        this.user = user;
+        this.serviceProvider = serviceProvider;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CountryName getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(CountryName countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+}
