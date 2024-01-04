4bf25e5b13b2d942ef2575d7af8a92720e1356f1
shivanikowkuri <147936662+shivanikowkuri@users.noreply.github.com>
2024-01-05 00:19:20 +0530

    Update ConnectionService.java

--- src/main/java/com/driver/services/ConnectionService.java
+++ src/main/java/com/driver/services/ConnectionService.java
@@ -1,11 +1,9 @@
 package com.driver.services;
 
 import com.driver.model.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface ConnectionService {
-    User connect(int userId, String countryName) throws Exception;
-    User disconnect(int userId) throws Exception;
-    User communicate(int senderId, int receiverId) throws Exception;
+    public User connect(int userId, String countryName) throws Exception;
+    public User disconnect(int userId) throws Exception;
+    public User communicate(int senderId, int receiverId) throws Exception;
 }
