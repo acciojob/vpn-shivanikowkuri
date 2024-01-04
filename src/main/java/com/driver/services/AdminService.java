5372ef80772119aeda4832174bfed7593626fb9c
shivanikowkuri <147936662+shivanikowkuri@users.noreply.github.com>
2024-01-05 00:18:53 +0530

    Update AdminService.java

--- src/main/java/com/driver/services/AdminService.java
+++ src/main/java/com/driver/services/AdminService.java
@@ -2,11 +2,10 @@ package com.driver.services;
 
 import com.driver.model.Admin;
 import com.driver.model.ServiceProvider;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface AdminService {
-    Admin register(String username,String password);
-    Admin addServiceProvider(int adminId, String providerName);
-    ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception;
+    public Admin register(String username,String password);
+    public Admin addServiceProvider(int adminId , String providerName);
+
+    public ServiceProvider addCountry(int serviceProviderId , String countryName) throws Exception;
 }
