068f4729b654729750614b511864a820c3d8b215
shivanikowkuri <147936662+shivanikowkuri@users.noreply.github.com>
2024-01-05 00:19:49 +0530

    Update UserService.java

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,10 +1,8 @@
 package com.driver.services;
 
 import com.driver.model.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
-    User register(String username, String password, String countryName) throws Exception;
-    User subscribe(Integer userId, Integer serviceProviderId);
+    public User register(String username, String password, String countryName) throws Exception;
+    public User subscribe(Integer userId, Integer serviceProviderId);
 }
